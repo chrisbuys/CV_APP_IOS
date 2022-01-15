17a45f77dc32ee60c2342e2d752ca1601d23dd3e
Chris <chrisbuys@me.com>
2022-01-15 13:11:29 +0100

    Initial Commit

---
+++ ChrisCard/ChrisCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChrisCardApp.swift
+//  ChrisCard
+//
+//  Created by Chris Buys on 15/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChrisCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
